d3b3c294ddbc6b237af832ed0387aac1e9ac3be4
school computer <10021902@sbstudents.org>
2022-02-03 12:16:00 -0500

    alternate location method

--- app/src/main/java/com/example/gpsapp/MainActivity.kt
+++ app/src/main/java/com/example/gpsapp/MainActivity.kt
@@ -1,78 +1,60 @@
 package com.example.gpsapp
 
+
 import android.Manifest
 import android.content.pm.PackageManager
 import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Looper
-import android.util.Log
+import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import com.example.gpsapp.databinding.ActivityMainBinding
-import com.google.android.gms.location.*
 import java.util.*
-
-private lateinit var fusedLocationClient: FusedLocationProviderClient
-private lateinit var locationRequest: LocationRequest
-private lateinit var locationCallback: LocationCallback
 
 private lateinit var binding: ActivityMainBinding
 
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Log.d("Tag", "oncreate")
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(binding.root.context)
-        getLocationUpdates()
 
-    }
-    private fun getLocationUpdates()
-    {
-        Log.d("Tag", "Get location updates")
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(binding.root.context!!)
-        Log.d("Tag", "Location Request")
-
-        locationRequest = LocationRequest()
-        locationRequest.interval = 500
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        locationCallback = object : LocationCallback() {
-            override fun onLocationResult(locationResult: LocationResult) {
-                Log.d("Tag", "callback")
-
-                locationResult ?: return
-                if (locationResult.locations.isNotEmpty()) {
-                    val location =
-                        locationResult.lastLocation
-                    binding.coord.text = "Latitude: ${location.latitude}, Longitude: ${location.longitude}"
-                    binding.textView.text = Geocoder(binding.root.context, Locale.US).getFromLocation(location.latitude,location.longitude,1)[0].getAddressLine(0)
-                }
-            }
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
         }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,100.toFloat(),this);
+
     }
 
-    private fun startLocationUpdates() {
-        Log.d("Tag", "start updates")
-        fusedLocationClient.requestLocationUpdates(
-            locationRequest, locationCallback, Looper.myLooper()!!
-        )
+//    private val locationListener: LocationListener =
+//        LocationListener { location ->
+//
+//        }
+
+    override fun onLocationChanged(location: Location) {
+        binding.coord.text = "Latitude: ${location.latitude.shorten(5)}, Longitude: ${location.longitude.shorten(5)}"
+        binding.textView.text = Geocoder(binding.root.context, Locale.US).getFromLocation(
+            location.latitude,
+            location.longitude,
+            1
+        )[0].getAddressLine(0)
     }
 
-    private fun stopLocationUpdates() {
-        Log.d("Tag", "stop updates")
-        fusedLocationClient.removeLocationUpdates(locationCallback)
-    }
+    override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+    override fun onProviderEnabled(provider: String) {}
+    override fun onProviderDisabled(provider: String) {}
+    fun Double.shorten(decimals: Int): Double{ return String.format("%.$decimals" + "f", this).replace(',', '.').toDouble() }
 
-    override fun onPause() {
-        super.onPause()
-        stopLocationUpdates()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        startLocationUpdates()
-    }
 }
